61c0c81e103b98a3fcc44588a7e70a5cca12868e
SoleP <79757978+SoleP@users.noreply.github.com>
2021-11-19 17:58:25 +0000

    Add files via upload

---
+++ pruebaJS.js
@@ -0,0 +1,19 @@
+// FUNCTION CODE MULTIPLZ TWO NUMBERS 
+// function addNumbers (num1, num2) {
+//     var result = num1 + num2;
+//     return result;
+// }
+
+// alert (addNumbers (9,7))
+
+//function will return the pruduct ot num 1 and 2 
+
+
+// CONDITIONAL STATEMENTS
+    var icecreamType = "pistachio";
+
+    if (icecreamType == "pitachio") {
+        alert ("Soy siciliano");
+    }else{
+        alert("Entonces dame super dulce de leche");
+    }
